cb4a6ecbb739e3a3e506a635fb3cbe1a06d41d29
Johnsensei <john@johnsensei.com>
2021-11-17 12:07:59 -0600

    Changed develop back

--- src/App.js
+++ src/App.js
@@ -4,12 +4,7 @@ import Main from "./components/Main";
 function App () {
     return(
       <Main/>
-
       // I changed something on develop.
-
-
-      // I changed something on dev1
-
     );
 }
 
