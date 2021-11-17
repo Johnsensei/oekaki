d9d7d5233d9cbd1c0f00fd13504a93d823e97e32
Johnsensei <john@johnsensei.com>
2021-11-17 12:14:04 -0600

    changed dev1 to make it different

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Main from "./components/Main";
 function App () {
     return(
       <Main/>
-      // I changed something on dev1
+      // I changed something on dev1 to make it different.
     );
 }
 
